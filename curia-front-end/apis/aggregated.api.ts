a23f401e4fceab4aa3b552fbee6564e5cbee268a
stevelw <steve@stevelw.co.uk>
2025-04-09 22:27:56 +0100

    add aggregated api

---
+++ curia-front-end/apis/aggregated.api.ts
@@ -0,0 +1,27 @@
+import * as vaApi from "./va.api";
+import * as metApi from "./met.api";
+
+export interface Artefact {
+  Title: string;
+}
+
+export function search(searchTerm: string): Promise<Artefact[]> {
+  return Promise.all([metApi.search(searchTerm), vaApi.search(searchTerm)])
+    .then(([metArtefacts, vaArtefacts]) => {
+      return [...metArtefacts, ...vaArtefacts].sort((a, b) => {
+        const titleA = a.Title.toUpperCase();
+        const titleB = b.Title.toUpperCase();
+        if (titleA < titleB) {
+          return -1;
+        }
+        if (titleA > titleB) {
+          return 1;
+        }
+
+        return 0;
+      });
+    })
+    .catch((err) => {
+      throw new Error(`API error: ${err}`);
+    });
+}
