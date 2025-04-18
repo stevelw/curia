75127a791b0f1f46c26ba8567c09f333b43215a6
stevelw <steve@stevelw.co.uk>
2025-04-18 22:41:13 +0100

    add artefact routing

---
+++ curia-front-end/src/app/artefact/[artefactId].tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default function ArtefactDetails() {
+  return (
+    <>
+      <h1 style={styles.h1}>Artefact details</h1>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  h1: {
+    padding: 10,
+  },
+});
