153357e56107928658faf738b2820fc50c3f6a74
stevelw <steve@stevelw.co.uk>
2025-04-27 14:41:58 +0100

    add tests

--- curia-back-end/test/app.e2e-spec.ts
+++ curia-back-end/test/app.e2e-spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
@@ -5,7 +7,6 @@ import { App } from "supertest/types";
 import { AppModule } from "./../src/app.module";
 import { seed } from "../db/seed";
 import testData from "../db/data/test-data/data.index";
-import { expect as expectAny } from "@jest/globals";
 import { client } from "../db/connection";
 
 let app: INestApplication<App>;
@@ -25,22 +26,38 @@ afterAll(() => {
   void client.close();
 });
 
-describe("AppController (e2e)", () => {
+describe("root", () => {
   it("/ (GET)", () => {
     return request(app.getHttpServer())
       .get("/")
       .expect(200)
       .expect("API documentation available at /api");
   });
+});
 
-  it("/users (POST)", () => {
+describe("/signup", () => {
+  it("(POST) - unique username", () => {
     return request(app.getHttpServer())
-      .post("/users")
+      .post("/signup")
       .send({ username: "johnsmith" })
       .expect(201)
-      .expect({
-        user: { username: "johnsmith" },
-        refreshToken: expectAny.any(String),
+      .then((res) => {
+        expect(res.body).toHaveProperty("user");
+        const user = res.body.user;
+        expect(user).toHaveProperty("_id");
+        expect(typeof user._id).toBe("string");
+        expect(res.body.user).toMatchObject({
+          username: "johnsmith",
+        });
+        expect(res.body).toHaveProperty("refreshToken");
+        expect(typeof res.body.refreshToken).toBe("string");
       });
+  });
+
+  it("(POST) - existing username", () => {
+    return request(app.getHttpServer())
+      .post("/signup")
+      .send({ username: "Steve" })
+      .expect(409);
   });
 });
