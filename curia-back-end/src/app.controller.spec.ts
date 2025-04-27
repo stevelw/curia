153b20eccdd0de4f77bf645a3763e567d38ad19a
stevelw <steve@stevelw.co.uk>
2025-04-27 08:07:44 +0100

    Replace template root endpoint with API link

--- curia-back-end/src/app.controller.spec.ts
+++ curia-back-end/src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+    it("should return link to API documentation", () => {
+      expect(appController.getApiLink()).toBe(
+        "API documentation available at /api",
+      );
     });
   });
 });
--- curia-back-end/src/app.controller.ts
+++ curia-back-end/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getApiLink(): string {
+    return this.appService.getApiLink();
   }
 }
