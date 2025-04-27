3974f68d97f1f57b6a72fcc6f9b9d27d235baa71
stevelw <steve@stevelw.co.uk>
2025-04-27 11:50:36 +0100

    migrate to dotenvx

--- curia-back-end/db/connection.js
+++ curia-back-end/db/connection.js
@@ -1,15 +1,4 @@
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import { MongoClient } from "mongodb";
-
-const ENV = process.env.NODE_ENV || "development";
-// eslint-disable-next-line no-redeclare
-const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-redeclare
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.MONGO_CONNECTION_STRING) {
   throw new Error(
@@ -23,7 +12,7 @@ if (!process.env.DB_NAME) {
 
 const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
 
-await client.connect();
+client.connect();
 
 const db = client.db(process.env.DB_NAME);
 
--- curia-back-end/eslint.config.mjs
+++ curia-back-end/eslint.config.mjs
@@ -30,7 +30,6 @@ export default tseslint.config(
       sourceType: "commonjs",
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
