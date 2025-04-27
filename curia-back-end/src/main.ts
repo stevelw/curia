7aa7ddd7b76fd3ce0de8ba8b9291c3e7fd877cb4
stevelw <steve@stevelw.co.uk>
2025-04-27 07:23:40 +0100

    add swagger endpoint

--- curia-back-end/src/main.ts
+++ curia-back-end/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Curia Back End")
+    .setDescription("The Curia Back End API description")
+    .setVersion("1.0")
+    .addTag("Curia")
+    .build();
+  const ducumentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, ducumentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
