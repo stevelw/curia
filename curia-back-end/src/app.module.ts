ee94bea293bb9b996ae147f0ac6d43b589c84777
stevelw <steve@stevelw.co.uk>
2025-04-28 14:51:18 +0100

    add user module

--- curia-back-end/src/app.module.ts
+++ curia-back-end/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UsersModule } from "./users/users.module";
+
+if (!process.env.MONGO_CONNECTION_STRING) {
+  throw new Error(
+    "MONGO_CONNECTION_STRING needs to be set for this environment.",
+  );
+}
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
