22f5a2f1e04eef172aa16620b3dd892c0b2ef36d
stevelw <steve@stevelw.co.uk>
2025-04-29 12:47:24 +0100

    import passport and jwt modules

--- curia-back-end/src/auth/auth.module.ts
+++ curia-back-end/src/auth/auth.module.ts
@@ -4,10 +4,14 @@ import { AuthService } from "./auth.service";
 import { UsersService } from "../users/users.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "../users/schemas/user.schema";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule.register({ defaultStrategy: "jwt" }),
+    JwtModule.register({ secret: "", signOptions: { expiresIn: 900 } }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UsersService],
