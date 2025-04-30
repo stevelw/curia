f4ea9ba9dccd052812b6ffc9855486fd20dc91c4
stevelw <steve@stevelw.co.uk>
2025-04-30 22:31:48 +0100

    refactor - extract artefacts list

--- curia-front-end/src/components/SearchResults.tsx
+++ curia-front-end/src/components/SearchResults.tsx
@@ -1,34 +1,19 @@
 import { useCallback, useEffect, useState } from "react";
-import { FlatList, ScrollView, StyleSheet, View } from "react-native";
-import CollectionObjectListItem from "./CollectionObjectListItem";
+import { StyleSheet } from "react-native";
 import { useQueries, UseQueryResult } from "@tanstack/react-query";
 import { vaApi, SearchFnReturn } from "../apis/va.api";
 import { metApi } from "../apis/met.api";
-import PagePicker from "./PagePicker";
 import SearchBox from "./SearchBox";
 import { SortOptions } from "../apis/api.class";
-import SortPicker from "./SortPicker";
-import FilterPicker, { FilterOptions } from "./FilterPicker";
-
-const RESULTS_PER_PAGE = 10;
-const MAX_TO_RENDER_PER_BATCH = 10; // 10
-const UPDATE_CELLS_BATCH_PERIOD = 50; // 50
-const INITIAL_NUM_TO_RENDER = 20; // 10
-const WINDOW_SIZE = 5; // 21
+import CollectionObjectList, { RESULTS_PER_PAGE } from "./CollectionObjectList";
+import { defaultFilterOptions, FilterOptions } from "./FilterPicker";
 
 export default function SearchResults() {
   const [searchTerm, setSearchTerm] = useState("");
   const [sortBy, setSortBy] = useState(SortOptions.Maker);
   const [page, setPage] = useState(1);
-  const [numberOfPages, setNumberOfPages] = useState(1);
-  const [filterOptions, setFilterOptions] = useState<FilterOptions>({
-    objectType: { valid: [], selected: [] },
-    currentLocation: { valid: [], selected: [] },
-    api: {
-      valid: [vaApi.name, metApi.name].sort(),
-      selected: [vaApi.name, metApi.name],
-    },
-  });
+  const [filterOptions, setFilterOptions] =
+    useState<FilterOptions>(defaultFilterOptions);
 
   const combineResults = useCallback(
     (results: UseQueryResult<SearchFnReturn, Error>[]) => {
@@ -126,14 +111,6 @@ export default function SearchResults() {
 
   useEffect(() => {
     if (!queryResults.pending) {
-      setNumberOfPages(
-        Math.ceil(queryResults.totalResultsAvailable / RESULTS_PER_PAGE),
-      );
-    }
-  }, [queryResults.pending, queryResults]);
-
-  useEffect(() => {
-    if (!queryResults.pending) {
       setFilterOptions((prevFilterOptions) => ({
         objectType: {
           valid: queryResults.objectTypes,
@@ -157,49 +134,25 @@ export default function SearchResults() {
   ]);
 
   useEffect(() => {
-    if (page > numberOfPages) setPage(numberOfPages);
-  }, [page, numberOfPages]);
+    console.log("queryResults is ", queryResults);
+  }, [queryResults]);
 
   return (
     <>
       <h1 style={styles.h1}>Search</h1>
       <SearchBox setSearchTerm={setSearchTerm} />
       {searchTerm !== "" && (
-        <>
-          <SortPicker sortBy={sortBy} setSortBy={setSortBy} />
-          <View style={styles.columns}>
-            <ScrollView style={styles.filters}>
-              <FilterPicker
-                filterOptions={filterOptions}
-                setFilterOptions={setFilterOptions}
-              />
-            </ScrollView>
-            <View style={styles.results}>
-              <PagePicker
-                currentPage={page}
-                numOfPages={numberOfPages}
-                setPageCbFn={(page) => {
-                  setPage(page);
-                }}
-              />
-              {queryResults.pending ? (
-                <p>Loading...</p>
-              ) : (
-                <FlatList
-                  data={queryResults.pending ? [] : queryResults.data}
-                  keyExtractor={(item) => item.localId}
-                  maxToRenderPerBatch={MAX_TO_RENDER_PER_BATCH}
-                  updateCellsBatchingPeriod={UPDATE_CELLS_BATCH_PERIOD}
-                  initialNumToRender={INITIAL_NUM_TO_RENDER}
-                  windowSize={WINDOW_SIZE}
-                  renderItem={({ item }) => (
-                    <CollectionObjectListItem item={item} />
-                  )}
-                />
-              )}
-            </View>
-          </View>
-        </>
+        <CollectionObjectList
+          queryResultsPending={queryResults.pending}
+          artefactsForPage={queryResults.data}
+          totalResultsAvailable={queryResults.totalResultsAvailable}
+          page={page}
+          setPage={setPage}
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+          filterOptions={filterOptions}
+          setFilterOptions={setFilterOptions}
+        />
       )}
     </>
   );
@@ -208,15 +161,5 @@ export default function SearchResults() {
 const styles = StyleSheet.create({
   h1: {
     padding: 10,
-  },
-  columns: {
-    flexDirection: "row",
-    flex: 1,
-  },
-  filters: {
-    flex: 1,
-  },
-  results: {
-    flex: 2,
   },
 });
