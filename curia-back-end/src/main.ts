307215aaab367120ed6eb5a642a3574426b59965
stevelw <steve@stevelw.co.uk>
2025-04-30 08:34:49 +0100

    enable cors inNestJS

--- curia-back-end/src/main.ts
+++ curia-back-end/src/main.ts
@@ -4,7 +4,7 @@ import "@dotenvx/dotenvx/config";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle("Curia")
