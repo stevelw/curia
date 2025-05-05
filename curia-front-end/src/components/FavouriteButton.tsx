fc741b987d651cece398e4885f3e562e79b73490
stevelw <steve@stevelw.co.uk>
2025-05-05 22:16:57 +0100

    swap favourite button for Button

--- curia-front-end/src/components/FavouriteButton.tsx
+++ curia-front-end/src/components/FavouriteButton.tsx
@@ -1,6 +1,6 @@
 import { LocalId } from "../apis/api.class";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import { MouseEventHandler, useCallback, useContext } from "react";
+import { Button } from "react-native";
+import { useCallback, useContext } from "react";
 import { addToFavourites, removeFromFavourites } from "../apis/backEnd.api";
 import { SessionContext } from "../contexts/session.context";
 interface Props {
@@ -10,61 +10,41 @@ interface Props {
 export default function FavouriteButton({ localId }: Props) {
   const [session, setSession] = useContext(SessionContext);
 
-  const handleAddFavourite = useCallback<MouseEventHandler>(
-    (e) => {
-      e.preventDefault();
-      const favourite = async () => {
-        const cachedFavourites = await addToFavourites(
-          session.accessToken,
-          localId,
-        );
-        setSession((prev) => ({ ...prev, cachedFavourites }));
-      };
-      void favourite();
-    },
-    [setSession, localId, session.accessToken],
-  );
+  const handleAddFavourite = useCallback(() => {
+    const favourite = async () => {
+      const cachedFavourites = await addToFavourites(
+        session.accessToken,
+        localId,
+      );
+      setSession((prev) => ({ ...prev, cachedFavourites }));
+    };
+    void favourite();
+  }, [setSession, localId, session.accessToken]);
 
-  const handleRemoveFavourite = useCallback<MouseEventHandler>(
-    (e) => {
-      e.preventDefault();
-      const favourite = async () => {
-        const cachedFavourites = await removeFromFavourites(
-          session.accessToken,
-          localId,
-        );
-        setSession((prev) => ({ ...prev, cachedFavourites }));
-      };
-      void favourite();
-    },
-    [setSession, localId, session.accessToken],
-  );
+  const handleRemoveFavourite = useCallback(() => {
+    const favourite = async () => {
+      const cachedFavourites = await removeFromFavourites(
+        session.accessToken,
+        localId,
+      );
+      setSession((prev) => ({ ...prev, cachedFavourites }));
+    };
+    void favourite();
+  }, [setSession, localId, session.accessToken]);
 
   if (!session.accessToken) return <> </>;
 
-  return (
-    <TouchableOpacity>
-      <button
-        onClick={
-          session.cachedFavourites?.includes(localId)
-            ? handleRemoveFavourite
-            : handleAddFavourite
-        }
-        style={
-          session.cachedFavourites?.includes(localId)
-            ? styles.isFavourited
-            : styles.isNotFavourited
-        }
-      >
-        {session.cachedFavourites?.includes(localId)
-          ? "Unfavourite"
-          : "Favourite"}
-      </button>
-    </TouchableOpacity>
-  );
+  if (session.cachedFavourites?.includes(localId)) {
+    return (
+      <Button
+        title="Unfavourite"
+        color={"red"}
+        onPress={handleRemoveFavourite}
+      />
+    );
+  } else {
+    return (
+      <Button title="Favourite" color={"gold"} onPress={handleAddFavourite} />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  isFavourited: { backgroundColor: "red" },
-  isNotFavourited: { backgroundColor: "gold" },
-});
