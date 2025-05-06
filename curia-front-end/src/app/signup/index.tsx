1a64dbe54aa759bc6bdd38e71dce0c76442d0daf
stevelw <steve@stevelw.co.uk>
2025-05-06 13:47:26 +0100

    make signup page responsive

--- curia-front-end/src/app/signup/index.tsx
+++ curia-front-end/src/app/signup/index.tsx
@@ -2,7 +2,7 @@ import { signup } from "@/src/apis/backEnd.api";
 import { SessionContext } from "@/src/contexts/session.context";
 import { useRouter } from "expo-router";
 import { FormEventHandler, useContext, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function Index() {
   const [_session, setSession] = useContext(SessionContext);
@@ -12,9 +12,8 @@ export default function Index() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmit = () => {
     setError("");
-    e.preventDefault();
     const createAccountAndSignIn = async () => {
       try {
         const accessToken = await signup(username, password);
@@ -34,10 +33,11 @@ export default function Index() {
   return (
     <View style={styles.view}>
       <form style={styles.container} onSubmit={handleSubmit}>
-        <div>
-          <label style={styles.item}>
+        <div style={styles.inputContainer}>
+          <label>
             Username
             <input
+              style={styles.input}
               type="text"
               id="username"
               value={username}
@@ -45,10 +45,11 @@ export default function Index() {
             />
           </label>
         </div>
-        <div>
-          <label style={styles.item}>
+        <div style={styles.inputContainer}>
+          <label>
             Password
             <input
+              style={styles.input}
               type="text"
               id="password"
               value={password}
@@ -56,13 +57,12 @@ export default function Index() {
             />
           </label>
         </div>
-        <button
-          type="submit"
-          style={styles.item}
+        <Button
+          title="Sign up"
+          onPress={handleSubmit}
           disabled={!username || !password}
-        >
-          Sign Up
-        </button>
+          color={"green"}
+        />
         {error && <p>{error}</p>}
       </form>
     </View>
@@ -75,10 +75,26 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   container: {
-    flex: 1,
+    display: "flex",
     flexDirection: "column",
+    height: "100%",
+    justifyContent: "center",
   },
-  item: {
-    flex: 1,
+  input: {
+    borderRadius: 5,
+    borderWidth: 0,
+    shadowColor: "grey",
+    shadowOpacity: 0.8,
+    shadowOffset: { width: 0, height: 3 },
+    shadowRadius: 10,
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    margin: 10,
+  },
+  inputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
   },
 });
